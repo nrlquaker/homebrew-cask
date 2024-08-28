62d94699b152b7ab7bfbd33299b2e3a76080fd91
nrlquaker <ustynov.vy@gmail.com>
2024-08-28 18:49:55 +0200

    Add ryujinx

---
+++ Casks/r/ryujinx.rb
@@ -0,0 +1,19 @@
+cask "ryujinx" do
+  version "1.1.1380"
+  sha256 "552b437be6e11a22290d5f93dc23feba7652c575e1f0f5e060b4045423f2089d"
+
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-master/"
+  name "ryujinx"
+  desc "Nintendo Switch emulator"
+  homepage "https://ryujinx.org/"
+
+  app "Ryujinx.app"
+
+  zap trash: [
+    "~/Library/Application Support/Ryujinx",
+    "~/Library/Logs/Ryujinx",
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
